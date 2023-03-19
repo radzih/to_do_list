aef1d118024d0417aea0d1c4b2f9441fe0727087
Vadim Radzih <iphonevadim2003@gmail.com>
2023-03-19 13:02:27 +0200

    Added CORS

--- src/presentation/api/middlewares/setup.py
+++ src/presentation/api/middlewares/setup.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from .db_session import DatabaseSessionMiddleware
@@ -9,4 +10,10 @@ def include_middlewares(
 ) -> None:
     app.add_middleware(
         DatabaseSessionMiddleware, session_factory=session_factory
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=("*",),
+        allow_methods=("*",),
+        allow_headers=("*",),
     )
